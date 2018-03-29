6c33cc9aa68549c8bdcd6753e135d42764f2aad4
RomainPe <23080067+RomainPe@users.noreply.github.com>
2018-03-29 15:40:31 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(name='evdetect',
+      packages=find_packages(),
       description='Audio event detection library',
       url='https://github.com/romainpe/audio-event-detection',
-      install_requires=['numpy','scipy','librosa'],
-      packages=find_packages())
+      install_requires=['numpy', 'scipy', 'librosa'],
+      python_requires='>=3.5')
