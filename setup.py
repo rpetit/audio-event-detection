b86d03867a4ba5bcb0af04019313eb254d868826
RomainPe <23080067+RomainPe@users.noreply.github.com>
2018-03-29 15:45:37 +0200

    remove python3 requirement

--- setup.py
+++ setup.py
@@ -4,5 +4,4 @@ setup(name='evdetect',
       packages=find_packages(),
       description='Audio event detection library',
       url='https://github.com/romainpe/audio-event-detection',
-      install_requires=['numpy', 'scipy', 'librosa'],
-      python_requires='>=3.5')
+      install_requires=['numpy', 'scipy', 'librosa'])
