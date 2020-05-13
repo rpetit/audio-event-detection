24668b76f09a4d86f28a7fdc210f60882076f388
Romain Petit <romain.petit@inria.fr>
2020-05-13 09:26:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(name='evdetect',
       packages=find_packages(),
       description='Audio event detection library',
-      url='https://github.com/romainpe/audio-event-detection',
+      url='https://github.com/rpetit/audio-event-detection',
       install_requires=['numpy', 'scipy', 'matplotlib', 'librosa'])
